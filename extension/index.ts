eae4df382987d9cedc9bc6bb7168e0ae92cbc131
Grzegorz Godlewski <gg@gitgis.com>
2026-07-28 19:08:51 +0200

    Bump deps, hide prod console

--- extension/index.ts
+++ extension/index.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 import { MarkdownEditorProvider } from './markdownEditorProvider.js';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(MarkdownEditorProvider.register(context));
+	MarkdownEditorProvider.register(context);
 }
 
 export function deactivate() {}
